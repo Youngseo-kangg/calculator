1e048a93e27d5f8d96c2d338298b9f201b95a724
Youngseo Kang <youngseo.kangg@gmail.com>
2021-10-08 23:11:15 +0900

    [HTML] 기본 구조 작성

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
 import './App.css';
 
 function App() {
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <div className='App'>
-      <header className='App-header'>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <div>
+          <div></div> {/* 숫자 입력한거 보이는 창 */}
+          <ul>
+            <li>ON</li>
+            <li>OFF</li>
+            <li>CA</li>
+            <li>=</li>
+          </ul>{' '}
+          {/* ON, OFF, CANCEL ALL, = */}
+          <div>
+            <ul>
+              {numbers.map((num) => {
+                return <li>{num}</li>;
+              })}
+            </ul>{' '}
+            {/* 숫자 창들 */}
+            <ul>
+              <li>+</li>
+              <li>-</li>
+              <li>&times;</li>
+              <li>.</li>
+            </ul>{' '}
+            {/* 더하기, 빼기, 곱하기, 나누기, 소수점 */}
+          </div>
+          {/* 숫자창, 더하기빼기 창 */}
+        </div>
+        {/* modal */}
+      </div>
+      {/* backdrop */}
     </div>
   );
 }
