c757223d7d12fb3fbf8cda5e75bf62b878e64283
Youngseo Kang <youngseo.kangg@gmail.com>
2021-10-10 23:34:56 +0900

    [tailwindCSS] 반응형 구현 및 custom 디자인 적용

--- src/App.js
+++ src/App.js
@@ -1,39 +1,35 @@
-import './App.css';
-
 function App() {
-  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'CA', '='];
   return (
-    <div className='App'>
-      <div>
-        <div>
-          <div></div> {/* 숫자 입력한거 보이는 창 */}
-          <ul>
-            <li>ON</li>
-            <li>OFF</li>
-            <li>CA</li>
-            <li>=</li>
-          </ul>{' '}
-          {/* ON, OFF, CANCEL ALL, = */}
-          <div>
-            <ul>
-              {numbers.map((num) => {
-                return <li>{num}</li>;
-              })}
-            </ul>{' '}
-            {/* 숫자 창들 */}
-            <ul>
-              <li>+</li>
-              <li>-</li>
-              <li>&times;</li>
-              <li>.</li>
-            </ul>{' '}
-            {/* 더하기, 빼기, 곱하기, 나누기, 소수점 */}
-          </div>
-          {/* 숫자창, 더하기빼기 창 */}
+    <div className='w-full h-screen min-w-full flex justify-center items-center bg-pink-50 font-display'>
+      <div className='flex flex-col w-full min-h-568 min-w-320 mobile:w-11/12 mobile:max-w-screen-mobile h-4/6 bg-pink-100'>
+        <div className='h-2/6 flex justify-center items-center'>02134</div>
+        <div className='flex w-full h-4/6 w-full'>
+          <ul className='flex flex-wrap w-3/4 m-1 box-border min-w-screen-mobile'>
+            {numbers.map((num) => {
+              return (
+                <li className='w-1/3 h-1/4 flex justify-center items-center bg-pink-50 rounded-full box-border shadow'>
+                  {num}
+                </li>
+              );
+            })}
+          </ul>
+          <ul className='flex flex-col w-1/4'>
+            <li className='h-1/4 bg-pink-200 m-1 rounded-full flex justify-center items-center box-border shadow'>
+              +
+            </li>
+            <li className='h-1/4 bg-pink-200 m-1 rounded-full flex justify-center items-center box-border shadow'>
+              -
+            </li>
+            <li className='h-1/4 bg-pink-200 m-1 rounded-full flex justify-center items-center box-border shadow'>
+              &times;
+            </li>
+            <li className='h-1/4 bg-pink-200 m-1 rounded-full flex justify-center items-center box-border shadow'>
+              .
+            </li>
+          </ul>
         </div>
-        {/* modal */}
       </div>
-      {/* backdrop */}
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      mobile: '320px',
+      tablet: '640px',
+      laptop: '1024px',
+    },
+    minHeight: {
+      568: '568px',
+    },
+    minWidth: {
+      320: '320px',
+      330: '330px',
+    },
+    fontFamily: {
+      display: ['LAB디지털', 'ui-serif'],
+    },
   },
   variants: {
     extend: {},
