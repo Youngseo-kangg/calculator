89588dfebc5172f0ddfc28e26e70f465c08b4c20
Youngseo Kang <youngseo.kangg@gmail.com>
2021-10-14 15:55:50 +0900

    [React] 계산 함수 수정

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   const [status, setStatus] = useState({
     numStat: false,
     screen: '0',
-    firstNum: '',
+    firstNum: '0',
     operator: '',
-    secondNum: '',
+    secondNum: '0',
     result: '',
   });
 
@@ -19,18 +19,45 @@ function App() {
       // TODO : 첫번째 수 입력하는 경우
       if (status.result) {
         // TODO : 계산한 적이 있을 때
-        setStatus({
-          ...status,
-          screen: currentNum, // 화면에 띄워주기
-          firstNum: status.firstNum + currentNum, // secondNum 기억하고 있기
-        });
-      } else if (!status.firstNum) {
+        if (currentNum === '.') {
+          // TODO : 계산한 적이 있을 때 + '.' 클릭했을 때
+          setStatus({
+            ...status,
+            screen: status.secondNum + currentNum, // '0.' 화면에 띄워주기
+            secondNum: status.secondNum + currentNum, // firstNum '0.'으로 기억하고 있기
+          });
+        } else if (status.secondNum === '0') {
+          // TODO : 계산한 적이 있을 때 + 숫자 클릭했을 때
+          setStatus({
+            ...status,
+            screen: currentNum,
+            secondNum: currentNum,
+          });
+        } else {
+          // TODO : 계산한 적이 있을 때 + 이외 숫자 클릭했을 때
+          setStatus({
+            ...status,
+            screen: status.secondNum + currentNum,
+            secondNum: status.secondNum + currentNum,
+          });
+        }
+      } else if (status.firstNum === '0') {
         // TODO : 아예 처음으로 입력할때
-        setStatus({
-          ...status,
-          screen: currentNum, // 화면에 0 대신 클릭한 숫자로 띄워주기
-          firstNum: currentNum, // firstNum 기억하고 있기
-        });
+        if (currentNum === '.') {
+          // TODO : 아예 처음으로 입력할때 -> '.' 클릭했을 때
+          setStatus({
+            ...status,
+            screen: status.firstNum + currentNum, // 화면에 0 대신 '0.'로 띄워주기
+            firstNum: status.firstNum + currentNum, // '0.'으로 나타나도록 하기
+          });
+        } else {
+          // TODO : 아예 처음으로 입력할때 -> 숫자 클릭했을 때
+          setStatus({
+            ...status,
+            screen: currentNum, // 화면에 0 대신 클릭한 숫자로 띄워주기
+            firstNum: currentNum, // firstNum 기억하고 있기
+          });
+        }
       } else {
         // TODO : 이후에 입력할때
         setStatus({
@@ -41,20 +68,31 @@ function App() {
       }
     } else {
       // TODO : 두번째 수 입력하는 경우
-      if (status.result) {
-        // TODO : 계산한 적이 있을 때
-        setStatus({
-          ...status,
-          screen: currentNum, // 화면에 띄워주기
-          firstNum: status.firstNum + currentNum, // secondNum 기억하고 있기
-        });
-      } else if (!status.secondNum) {
+      // if (status.result) {
+      //   // TODO : 계산한 적이 있을 때
+      //   setStatus({
+      //     ...status,
+      //     screen: currentNum, // 화면에 띄워주기
+      //     firstNum: status.firstNum + currentNum, // secondNum 기억하고 있기
+      //   });
+      // } else
+      if (status.secondNum === '0') {
         // TODO : 아예 처음으로 입력할때
-        setStatus({
-          ...status,
-          screen: currentNum, // 화면에 띄워주기
-          secondNum: status.secondNum + currentNum, // secondNum 기억하고 있기
-        });
+        if (currentNum === '.') {
+          // TODO : 아예 처음으로 입력할때 -> '.' 클릭했을 때
+          setStatus({
+            ...status,
+            screen: status.secondNum + currentNum, // 화면에 0 대신 클릭한 숫자로 띄워주기
+            secondNum: status.secondNum + currentNum, // '0.'으로 나타나도록 하기
+          });
+        } else {
+          // TODO : 아예 처음으로 입력할때 -> 숫자 클릭했을 때
+          setStatus({
+            ...status,
+            screen: currentNum, // 화면에 0 대신 클릭한 숫자로 띄워주기
+            secondNum: currentNum, // firstNum 기억하고 있기
+          });
+        }
       } else {
         // TODO : 이후에 입력할때
         setStatus({
@@ -72,7 +110,6 @@ function App() {
     setStatus({
       ...status,
       numStat: !status.numStat, // TODO : numStat 변경해주기 -> 이래야 첫번째수인지, 두번째인지 알게됨
-      screen: '', // TODO : screen 빈칸 처리
       operator: currOperator, // TODO : 계산자 입력받아서 기억하고 있기
     });
   };
@@ -80,86 +117,41 @@ function App() {
   // 계산하는 함수 changeResult
   const changeResult = () => {
     if (status.operator === '+') {
-      if (status.result !== '') {
-        // TODO : 이전에 계산 기록이 있다면
-        setStatus({
-          ...status,
-          result: parseFloat(status.result) + parseFloat(status.firstNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.result) + parseFloat(status.firstNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      } else {
-        setStatus({
-          ...status,
-          result: parseFloat(status.firstNum) + parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.firstNum) + parseFloat(status.secondNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      }
+      setStatus({
+        ...status,
+        result: parseFloat(status.firstNum) + parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
+        screen: parseFloat(status.firstNum) + parseFloat(status.secondNum), // TODO : 스크린에 보여주기
+        firstNum: parseFloat(status.firstNum) + parseFloat(status.secondNum), // TODO : firstNum 빈칸으로 바꿔주기
+        secondNum: '0', // TODO : secondNum 빈칸으로 바꿔주기
+        numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
+      });
     } else if (status.operator === '-') {
-      if (status.result !== '') {
-        setStatus({
-          ...status,
-          result: parseFloat(status.result) - parseFloat(status.firstNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.result) - parseFloat(status.firstNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      } else {
-        setStatus({
-          ...status,
-          result: parseFloat(status.firstNum) - parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.firstNum) - parseFloat(status.secondNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      }
+      setStatus({
+        ...status,
+        result: parseFloat(status.firstNum) - parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
+        screen: parseFloat(status.firstNum) - parseFloat(status.secondNum), // TODO : 스크린에 보여주기
+        firstNum: parseFloat(status.firstNum) - parseFloat(status.secondNum), // TODO : firstNum 빈칸으로 바꿔주기
+        secondNum: '0', // TODO : secondNum 빈칸으로 바꿔주기
+        numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
+      });
     } else if (status.operator === '×') {
-      if (status.result !== '') {
-        setStatus({
-          ...status,
-          result: parseFloat(status.result) * parseFloat(status.firstNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.result) * parseFloat(status.firstNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      } else {
-        setStatus({
-          ...status,
-          result: parseFloat(status.firstNum) * parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.firstNum) * parseFloat(status.secondNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      }
+      setStatus({
+        ...status,
+        result: parseFloat(status.firstNum) * parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
+        screen: parseFloat(status.firstNum) * parseFloat(status.secondNum), // TODO : 스크린에 보여주기
+        firstNum: parseFloat(status.firstNum) * parseFloat(status.secondNum), // TODO : firstNum 빈칸으로 바꿔주기
+        secondNum: '0', // TODO : secondNum 빈칸으로 바꿔주기
+        numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
+      });
     } else if (status.operator === '/') {
-      if (status.result !== '') {
-        setStatus({
-          ...status,
-          result: parseFloat(status.result) / parseFloat(status.firstNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.result) / parseFloat(status.firstNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      } else {
-        setStatus({
-          ...status,
-          result: parseFloat(status.firstNum) / parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
-          screen: parseFloat(status.firstNum) / parseFloat(status.secondNum), // TODO : 스크린에 보여주기
-          firstNum: '', // TODO : firstNum 빈칸으로 바꿔주기
-          secondNum: '', // TODO : secondNum 빈칸으로 바꿔주기
-          numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
-        });
-      }
+      setStatus({
+        ...status,
+        result: parseFloat(status.firstNum) / parseFloat(status.secondNum), // TODO : 결과값 가지고 있기
+        screen: parseFloat(status.firstNum) / parseFloat(status.secondNum), // TODO : 스크린에 보여주기
+        firstNum: parseFloat(status.firstNum) / parseFloat(status.secondNum), // TODO : firstNum 빈칸으로 바꿔주기
+        secondNum: '0', // TODO : secondNum 빈칸으로 바꿔주기
+        numStat: false, // TODO : numStat 변경해서 앞으로 입력할 값 2번째값으로 되도록 변경
+      });
     }
   };
 
